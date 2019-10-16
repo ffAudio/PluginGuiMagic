16c34668f937ad1575477fca1b62218d76e001f3
Daniel Walz <developer@foleysfinest.com>
2019-10-16 22:57:34 +0100

    Updated docs

--- docs/search/classes_4.js
+++ docs/search/classes_4.js
@@ -4,7 +4,6 @@ var searchData=
   ['magicbuilder',['MagicBuilder',['../classfoleys_1_1MagicBuilder.html',1,'foleys']]],
   ['magicfilterplot',['MagicFilterPlot',['../classfoleys_1_1MagicFilterPlot.html',1,'foleys']]],
   ['magicguibuilder',['MagicGUIBuilder',['../classfoleys_1_1MagicGUIBuilder.html',1,'foleys']]],
-  ['magicguibuilder_3c_20juce_3a_3aaudioprocessor_20_3e',['MagicGUIBuilder&lt; juce::AudioProcessor &gt;',['../classfoleys_1_1MagicGUIBuilder.html',1,'foleys']]],
   ['magiclevelsource',['MagicLevelSource',['../classfoleys_1_1MagicLevelSource.html',1,'foleys']]],
   ['magicoscilloscope',['MagicOscilloscope',['../classfoleys_1_1MagicOscilloscope.html',1,'foleys']]],
   ['magicplotcomponent',['MagicPlotComponent',['../classfoleys_1_1MagicPlotComponent.html',1,'foleys']]],
--- docs/search/functions_9.js
+++ docs/search/functions_9.js
@@ -1,13 +1,13 @@
 var searchData=
 [
   ['magicanalyser',['MagicAnalyser',['../classfoleys_1_1MagicAnalyser.html#aef7fe54dcf22511fab3b456a9381f508',1,'foleys::MagicAnalyser']]],
-  ['magicbuilder',['MagicBuilder',['../classfoleys_1_1MagicBuilder.html#a95f02e34cfcf8cf966c22bdf4a475be1',1,'foleys::MagicBuilder']]],
+  ['magicbuilder',['MagicBuilder',['../classfoleys_1_1MagicBuilder.html#a836b337031f412caf93815e123f164f2',1,'foleys::MagicBuilder']]],
   ['magicfilterplot',['MagicFilterPlot',['../classfoleys_1_1MagicFilterPlot.html#a5eb6ead6df89e85d9bd7814a4919602c',1,'foleys::MagicFilterPlot']]],
-  ['magicguibuilder',['MagicGUIBuilder',['../classfoleys_1_1MagicGUIBuilder.html#a7df0ae8d786a72ff73639e6ebade9eeb',1,'foleys::MagicGUIBuilder']]],
+  ['magicguibuilder',['MagicGUIBuilder',['../classfoleys_1_1MagicGUIBuilder.html#aca1ee02b7299f519c55cc3866db89a78',1,'foleys::MagicGUIBuilder']]],
   ['magicoscilloscope',['MagicOscilloscope',['../classfoleys_1_1MagicOscilloscope.html#a8830e06f111cf95157a7ae40b8886cbe',1,'foleys::MagicOscilloscope']]],
   ['magicplotcomponent',['MagicPlotComponent',['../classfoleys_1_1MagicPlotComponent.html#a8bb20c3d03ce8cc74d7091bc35b2b43a',1,'foleys::MagicPlotComponent']]],
   ['magicplotsource',['MagicPlotSource',['../classfoleys_1_1MagicPlotSource.html#a55bbde130cac6785e2d29b629fd7f7dd',1,'foleys::MagicPlotSource']]],
-  ['magicplugineditor',['MagicPluginEditor',['../classfoleys_1_1MagicPluginEditor.html#ab0d028ccc813056bc49cc6089f8afcfd',1,'foleys::MagicPluginEditor::MagicPluginEditor(MagicProcessorState &amp;processorState)'],['../classfoleys_1_1MagicPluginEditor.html#af2cbf6b43605d292c883973611aac3f8',1,'foleys::MagicPluginEditor::MagicPluginEditor(MagicProcessorState &amp;processorState, const char *data, const int dataSize)']]],
+  ['magicplugineditor',['MagicPluginEditor',['../classfoleys_1_1MagicPluginEditor.html#ad45b3d91f77af4e1344c9ca9bf9a884b',1,'foleys::MagicPluginEditor::MagicPluginEditor(MagicProcessorState &amp;processorState, std::unique_ptr&lt; MagicBuilder &gt; builder={})'],['../classfoleys_1_1MagicPluginEditor.html#ade4eee16b3a67e3defc65caeb30a6630',1,'foleys::MagicPluginEditor::MagicPluginEditor(MagicProcessorState &amp;processorState, const char *data, const int dataSize, std::unique_ptr&lt; MagicBuilder &gt; builder={})']]],
   ['magicprocessorstate',['MagicProcessorState',['../classfoleys_1_1MagicProcessorState.html#aeccdd2ffdb103851bc31512d5c516039',1,'foleys::MagicProcessorState']]],
   ['mousedoubleclick',['mouseDoubleClick',['../classfoleys_1_1StylePropertyComponent.html#a157094d9b4f4477360601d2ecbaaafaf',1,'foleys::StylePropertyComponent']]]
 ];
